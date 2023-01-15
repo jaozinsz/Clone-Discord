1cd4d5f672d2f57e9bad6eafca81952b0823cc1b
João Pedro <joaopedronunescunha7@gmail.com>
2023-01-15 01:03:44 -0300

    Page3 e Page4 feitas

--- src/App.js
+++ src/App.js
@@ -1,63 +1,128 @@
-import './App.scss';
-import logo from "./images/logo.png"
-import back1 from "./images/back1.svg"
-import back2 from "./images/back2.svg"
-import back3 from "./images/background.svg"
+import "./App.scss";
+import logo from "./images/logo.png";
+import back1 from "./images/back1.svg";
+import back2 from "./images/back2.svg";
+import back3 from "./images/background.svg";
 
-import img1 from "./images/img1.svg"
+import img1 from "./images/img1.svg";
+import img2 from "./images/img2.svg";
+import img3 from "./images/img3.svg";
 
 function App() {
   return (
-    <main className='Landing'>
-      
-      <section className='Page1'>
-        <header className='cabecalho'>
-          <img className='logo' src={logo} alt="logo" />
+    <main className="Landing">
+      <section className="Page1">
+        <header className="cabecalho">
+          <img className="logo" src={logo} alt="logo" />
 
-            <div className='links'>
+          <div className="links">
+            <a className="link" href="">
+              Download
+            </a>
 
-            <a className='link' href="">Download</a>
-            <a className='link' href="">Nitro</a>
-            <a className='link' href="">Discover</a>
-            <a className='link' href="">Safety</a>
-            <a className='link' href="">Support</a>
-            <a className='link' href="">Blog</a>
-            <a className='link' href="">Careers</a>
+            <a className="link" href="">
+              Nitro
+            </a>
+
+            <a className="link" href="">
+              Discover
+            </a>
+
+            <a className="link" href="">
+              Safety
+            </a>
+
+            <a className="link" href="">
+              Support
+            </a>
+
+            <a className="link" href="">
+              Blog
+            </a>
+
+            <a className="link" href="">
+              Careers
+            </a>
             
-            </div>
+          </div>
 
-          <button className='abrir'>Open Discord</button>
+          <button className="abrir">Open Discord</button>
         </header>
 
+        <img className="back3" src={back3} />
+        <img className="back1" src={back1} />
+        <img className="back2" src={back2} />
 
-        <img className='back3' src={back3} />
-        <img className='back1' src={back1} />
-        <img className='back2' src={back2} />
-        
+        <div className="textos">
+          <h1 className="titulo">IMAGINE UM LUGAR...</h1>
 
-        <div className='textos'>
-          <h1 className='titulo'>IMAGINE UM LUGAR...</h1>
+          <div className="sub">
+            <h5 className="subtitulo">
+              …onde você possa pertencer a um clube escolar, um grupo de gamers,
+              ou uma comunidade artística mundial. Onde você e alguns amigos
+              possam passar um tempo juntos. Um lugar que torna fácil conversar
+              todos os dias e socializar com mais frequência.
+            </h5>
+          </div>
 
-        <div className='sub'>
-          <h5 className='subtitulo'>…onde você possa pertencer a um clube escolar, um grupo de gamers, ou uma comunidade artística mundial. Onde você e alguns amigos possam passar um tempo juntos. Um lugar que torna fácil conversar todos os dias e socializar com mais frequência.</h5>
-        </div>
-
-        <div className='botoes'>
-          <button className='botao1'>Baixar para Windows</button>
-          <button className='botao2'>Abra o Discord no seu navegador</button>
-        </div>
+          <div className="botoes">
+            <button className="botao1">Baixar para Windows</button>
+            <button className="botao2">Abra o Discord no seu navegador</button>
+          </div>
         </div>
       </section>
 
-      <section className='Page2'>
-        <div className='image'>
+      <section className="Page2">
+        <div className="image">
           <img src={img1} alt="" />
         </div>
-        <div className='texto'>
-          <h1 className='titulo1'>Crie um espaço controlado por convite onde você se sinta em casa</h1>
+        <div className="texto">
+          <h1 className="titulo1">
+            Crie um espaço controlado por convite onde você se sinta em casa
+          </h1>
 
-          <h5 className='subtitulo1'>Os servidores Discord são organizados em canais com assuntos para vocês colaborarem, compartilharem ou simplesmente falarem do dia sem entupir um chat geral.</h5>
+          <h5 className="subtitulo1">
+            Os servidores Discord são organizados em canais com assuntos para
+            vocês colaborarem, compartilharem ou simplesmente falarem do dia sem
+            entupir um chat geral.
+          </h5>
         </div>
+      </section>
+
+      <section className="Page3">
+      <div className="image2">
+          <img src={img2} alt="" />
+        </div>
+        <div className="texto2">
+          <h1 className="titulo2">
+            Aqui é fácil se encontrar
+          </h1>
+
+          <h5 className="subtitulo2">
+            Os servidores Discord são organizados em canais com assuntos para
+            vocês colaborarem, compartilharem ou simplesmente falarem do dia sem
+            entupir um chat geral.
+          </h5>
+        </div>
+      </section>
+
+      <section className="Page4">
+        <div className="image3">
+          <img src={img3} alt="" />
+        </div>
+        <div className="texto3">
+          <h1 className="titulo3">
+            Para poucos e muitos amigos
+          </h1>
+
+          <h5 className="subtitulo3">
+          Organize qualquer comunidade com ferramentas de moderação e acesso personalizado a membros. Dê poderes especiais aos membros, monte canais privados e muito mais.
+          </h5>
+        </div>
+      </section>
+
+      <section className="Page5">
+
       </section>
     </main>
   );
